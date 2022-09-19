57b15dbe9dd03d54d54e0f91858c0e0915bae359
tyh2001 <1469442737@qq.com>
2022-09-19 16:32:34 +0800

    fest: 优化代码

--- miniprogram/pages/home/home.ts
+++ miniprogram/pages/home/home.ts
@@ -1,5 +1,5 @@
 // pages/home/home.ts
-import { dayMonth, dayWeek } from '../../utils/util'
+import { dayMonth, dayWeek, addPrefix } from '../../utils/util'
 import { lunarCalendar } from '../../utils/lunar'
 import type { kunarInterface } from '../../_interface/lunar'
 
@@ -12,54 +12,91 @@ Page({
    */
   data: {
     year: date.getFullYear(), // 年
-    month: date.getMonth(), // 月
+    month: date.getMonth() + 1, // 月
     date: date.getDate(), // 日
     dayMonth: 0, // 每个月有多少天
     weekList: ['日', '一', '二', '三', '四', '五', '六'] as const, // 周几
     dailyDetailAll: [] as kunarInterface[], // 详细描述信息
     dayWeekNum: 0, // 每月首日是星期几
-    dayItemWidth: 0 // 日期每一项的宽度
+    detailDate: '' // 详细日期
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad() {
+  // 上个月
+  prevMonth() {
+    if (this.data.month > 1) {
+      this.data.month--
+      this.calendarLoad()
+      return
+    }
+    this.data.year--
+    this.data.month = 12
+    this.calendarLoad()
+  },
+
+  // 返回今天
+  goTaday() {
+    this.data.month = date.getMonth() + 1
+    this.data.year = date.getFullYear()
+    this.data.date = date.getDate()
+    this.calendarLoad()
+  },
+
+  // 下个月
+  nextMoth() {
+    if (this.data.month < 12) {
+      this.data.month++
+      this.calendarLoad()
+      return
+    }
+    this.data.year++
+    this.data.month = 1
+    this.calendarLoad()
+  },
+
+  // 初始化渲染
+  calendarLoad() {
     const monthNum: number = dayMonth(this.data.year, this.data.month) // 获取这个月有多少天
     const dailyDetailAll: kunarInterface[] = [] // 每天的详细信息
 
     for (let i = 0; i < monthNum; i++) {
       const dailyDetai: kunarInterface = lunarCalendar.solar2lunar(
         this.data.year,
-        this.data.month + 1,
+        this.data.month,
         i + 1
       ) as kunarInterface
 
       dailyDetai.todayDate = i + 1 // 添加每天的日期
 
       // 周年日
-      if (this.data.month + 1 === 8 && dailyDetai.todayDate === 15) {
+      if (this.data.month === 8 && dailyDetai.todayDate === 15) {
         dailyDetai.festival = '周年日'
       }
 
       // 甜甜生日
-      if (this.data.month + 1 === 9 && dailyDetai.todayDate === 17) {
+      if (this.data.month === 9 && dailyDetai.todayDate === 17) {
         dailyDetai.festival = '甜甜生日'
       }
 
       // 维维生日
-      if (this.data.month + 1 === 10 && dailyDetai.todayDate === 18) {
+      if (this.data.month === 10 && dailyDetai.todayDate === 18) {
         dailyDetai.festival = '维维生日'
       }
-
       dailyDetailAll.push(dailyDetai)
     }
 
     this.setData({
       dayMonth: dayMonth(this.data.year, this.data.month), // 每个月有多少天
       dailyDetailAll, // 详细描述信息
-      dayWeekNum: dayWeek(this.data.year, this.data.month) // // 每月首日是星期几
+      dayWeekNum: dayWeek(this.data.year, this.data.month), // 每月首日是星期几
+      detailDate: `${this.data.year}年 ${addPrefix(this.data.month)}月 ${addPrefix(this.data.date)}日` // 详细信息
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+    this.calendarLoad()
   },
 
   /**
--- miniprogram/utils/util.ts
+++ miniprogram/utils/util.ts
@@ -1,14 +1,19 @@
 // 获取每个月多少天
 export const dayMonth = (year: number, month: number): number => {
-  if (month !== 1) {
+  if (month !== 2) {
     const months = [31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] as const
-    return months[month]
+    return months[month - 1]
   }
   return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? 29 : 28
 }
 
 // 计算当月的第一天是周几，返回需要该空出的格数
 export const dayWeek = (year: number, month: number): number => {
-  const firstDayWeek: number = new Date(`${year}/${month + 1}/1`).getDay()
+  const firstDayWeek: number = new Date(`${year}/${month}/1`).getDay()
   return firstDayWeek === 0 ? 0 : firstDayWeek
+}
+
+// 给数字前面加 0
+export const addPrefix = (num: number): string => {
+  return num > 9 ? num.toString() : `0${num}`
 }
